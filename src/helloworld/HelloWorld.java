df6bdecb4a5b469099cecf0f762712b1f7ca6ae8
home <home@home-PC>
2015-07-30 15:42:08 +0700

    add function doNothing

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,5 +3,9 @@ package helloworld;
 public class HelloWorld {
     public static void main(String args[]){
         System.out.print("Hello: " + args[0]);
+        return;
+    }
+    public static void doNothingFunc(){
+        //TODO
     }
 }
